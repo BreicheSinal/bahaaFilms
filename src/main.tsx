dae7507448e59c9f85c93bf8df46d167644c0f13
BreicheSinal <sinalbreiche@gmail.com>
2025-12-31 01:29:52 +0200

    updated router utils

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
+import "./styles/tokens.css";
 import "./styles/globals.css";
 import "./index.css";
 
