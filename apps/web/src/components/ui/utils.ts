197e5cb07aa86d8b4dad7af2b533cde6e62ca2c0
Sinal Breiche <sinalbreiche@gmail.com>
2026-05-05 01:01:35 +0300

    chore(ui): remove unused shadcn component implementations

--- apps/web/src/components/ui/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * Combines conditional class values and resolves Tailwind conflicts.
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
