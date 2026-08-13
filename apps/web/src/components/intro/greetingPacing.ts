db10f4e3210d9ba5d7947dbee28141cd4850f2a9
BreicheSinal <sinalbreiche@gmail.com>
2026-08-13 22:58:36 +0300

    tune(web): accelerate final intro burst

--- apps/web/src/components/intro/greetingPacing.ts
+++ apps/web/src/components/intro/greetingPacing.ts
@@ -1,5 +1,5 @@
 const GREETING_DELAYS = [800, 700, 600, 500, 400, 300];
 
 export function getGreetingDelay(index: number) {
-  return GREETING_DELAYS[index] ?? 180;
+  return GREETING_DELAYS[index] ?? 100;
 }
